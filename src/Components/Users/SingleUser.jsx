7c6aa4a406b39d1607e4b02dc486d1c98d37594b
md-huda-mia <mdhudamia.dev@gmail.com>
2023-11-28 18:10:16 +0600

    bg-color_update

--- src/Components/Users/SingleUser.jsx
+++ src/Components/Users/SingleUser.jsx
@@ -3,12 +3,10 @@ import React from "react";
 const SingleUser = ({ user }) => {
   const { name, email, phone } = user;
   return (
-    <div>
-      <div className="post_content">
-        <h3>Name: {name}</h3>
-        <p>Email: {email}</p>
-        <p>Phone: {phone}</p>
-      </div>
+    <div className="post_content">
+      <h3>Name: {name}</h3>
+      <p>Email: {email}</p>
+      <p>Phone: {phone}</p>
     </div>
   );
 };
--- src/Components/singleComm/SingleCom.jsx
+++ src/Components/singleComm/SingleCom.jsx
@@ -3,13 +3,11 @@ import React from "react";
 const SingleCom = ({ comment }) => {
   const { id, name, email, body } = comment;
   return (
-    <div>
-      <div className="post_content">
-        <span>UserId: {id}</span>
-        <h3>{name}</h3>
-        <h3>{email}</h3>
-        <p>{body}</p>
-      </div>
+    <div className="post_content">
+      <span>UserId: {id}</span>
+      <h3>{name}</h3>
+      <h3>{email}</h3>
+      <p>{body}</p>
     </div>
   );
 };
